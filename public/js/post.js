8975636699ff7d95648b7672ca2ba09b14235ec9
Mark Artim <mark.artim@gmail.com>
2021-05-10 18:10:27 -0500

    connect to heroku and jawsdb

--- public/js/post.js
+++ public/js/post.js
@@ -22,7 +22,29 @@ const postCommentFormHandler = async (event) => {
             };
 };
 
+// UPDATE POST or PUT a POST (funny huh?)
+const editPostFormHandler = async (event) => {
+    console.log("I am supposed to update a fucking post.")
+    event.preventDefault();
+    const url = window.location.pathname;
+    const id = url.substring(url.lastIndexOf('/') + 1)
+  
+    const title = document.querySelector('#title').value.trim();
+    const description = document.querySelector('#description').value.trim();
+    const post_id = id;
 
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, description }),
+        headers: { 'Content-Type': 'application/json' },
+        });
+
+    if (response.ok) {
+        document.location.replace('homepage');
+        } else {
+            alert(response.statusText);
+            };
+};
 
   
 const deletePostFormHandler = async (event) => {
@@ -54,4 +76,7 @@ document
 document
 .querySelector('#deletePost')
 .addEventListener('click', deletePostFormHandler);
+document
+.querySelector('#edit-post-form')
+.addEventListener('submit', editPostFormHandler);
 
