189104f7ef777b30c7c111c53112d0947ae5d047
Mark Artim <mark.artim@gmail.com>
2021-05-10 22:12:31 -0500

    change session timeout from 1 minute to 60 minutes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {maxAge: 60000},
+    cookie: {maxAge: 3600000},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
